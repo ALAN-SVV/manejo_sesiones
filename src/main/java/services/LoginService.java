189524942d58cebe9f29ebabc4c3a13ed9f09a31
Alan <alansvela2019@gmail.com>
2025-05-19 10:01:52 -0500

    a

--- src/main/java/services/LoginService.java
+++ src/main/java/services/LoginService.java
@@ -1,4 +1,9 @@
-package service;
+package services;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
 
 public interface LoginService {
+    Optional<String> getUserName(HttpServletRequest request);
+
 }
