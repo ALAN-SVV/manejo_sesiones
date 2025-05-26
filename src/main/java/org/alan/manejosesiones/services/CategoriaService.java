26492bcc7b294f9ad9e3460614ad39d8f18023de
Alan <alansvela2019@gmail.com>
2025-05-26 12:16:36 -0500

    conexion base de datos(2)

---
+++ src/main/java/org/alan/manejosesiones/services/CategoriaService.java
@@ -0,0 +1,9 @@
+package org.alan.manejosesiones.services;
+
+import org.alan.manejosesiones.models.Categorias;
+import java.util.List;
+
+public interface CategoriaService {
+    // Método para obtener la lista de productos
+    List<Categorias>listar();
+}
